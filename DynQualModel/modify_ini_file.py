502d16a8c0b65b5fb0eb947055a01310e56c777f
Edwin Sutanudjaja <edwinkost@UU105378>
2023-11-07 12:58:17 +0100

    test job on snellius

---
+++ DynQualModel/modify_ini_file.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+import os
+import sys
+import datetime
+import glob
+import shutil
+
+import pcraster as pcr
+from pcraster.framework import DynamicModel
+from pcraster.framework import DynamicFramework
+
+from configuration_for_modflow import Configuration
+from currTimeStep import ModelTime
+
+try:
+    from reporting_for_modflow import Reporting
+except:
+    pass
+
+try:
+    from modflow import ModflowCoupling
+except:
+    pass
+
+import virtualOS as vos
+
+import logging
+logger = logging.getLogger(__name__)
+
+import disclaimer
+
+def modify_ini_file(original_ini_file,
+                    system_argument): 
+
+    # created by Edwin H. Sutanudjaja on August 2020 for the Ulysses project
+    
+    # open and read ini file
+    file_ini = open(original_ini_file, "rt")
+    file_ini_content = file_ini.read()
+    file_ini.close()
+    
+    # a dictionary containing the system argument codes (e.g. -xyz, -abc) and their variable names (e.g. DEFGH and OPQRSTU)
+    ini_variables = {}
+    ini_variables["-mod"]           = "OUTPUT_FOLDER"
+    ini_variables["-sd"]            = "START_DATE"
+    ini_variables["-ed"]            = "END_DATE"
+    ini_variables["-misd"]          = "INITIAL_CONDITION_FOLDER"
+    ini_variables["-dfis"]          = "DATE_FOR_INITIAL_STATES"
+    ini_variables["-pff"]           = "PRE_FILE"
+    ini_variables["-tff"]           = "TMP_FILE"
+    ini_variables["-rpetff"]        = "ET0_FILE"
+    ini_variables["-radff"]         = "RAD_FILE"
+    ini_variables["-tmp_annavg_ff"] = "TMP_ANNUAL_AVG_FILE"
+    ini_variables["-irr_fl"]        = "IRRIGATION_AREA_FILE"
+    ini_variables["-dom_fl"]        = "DOM_WATER_DEMAND_FILE"
+    ini_variables["-ind_fl"]        = "IND_WATER_DEMAND_FILE"
+    
+    for var in ini_variables.keys{}
+         
+         # for the output directory
+         if var == "-mod": output_dir = assigned_string
+
+             # parallel option
+             this_run_is_part_of_a_set_of_parallel_run = False    
+             if system_argument[2] == "parallel" or system_argument[2] == "debug_parallel": this_run_is_part_of_a_set_of_parallel_run = True
+             
+             # for a parallel run (usually 5 arcmin), we assign the following based on the clone number/code:
+             if this_run_is_part_of_a_set_of_parallel_run:
+                 # set the output directory for every clone
+                 output_dir = output_dir + "/M" + clone_code + "/"
+
+         assigned_string  = system_argument[system_argument.index(var) + 1]
+         file_ini_content = file_ini_content.replace(ini_variables[var], assigned_string)
+         msg = "The string " + str(ini_variables[var) " in the given configuration file is replaced with the one given after the system argument " +  str(var) + ": "  + assigned_string + "."
+         print(msg)
+
+
+    # folder for saving original and modified ini files
+    folder_for_ini_files = os.path.join(output_dir, "ini_files")
+    # - create folder
+    if os.path.exists(folder_for_ini_files): shutil.rmtree(folder_for_ini_files)
+    os.makedirs(folder_for_ini_files)
+    
+    # save/copy the original ini file
+    shutil.copy(original_ini_file, os.path.join(folder_for_ini_files, os.path.basename(original_ini_file) + ".original"))
+
+    # save the new ini file
+    new_ini_file_name = os.path.join(folder_for_ini_files, os.path.basename(original_ini_file) + ".modified_and_used")
+    new_ini_file = open(new_ini_file_name, "w")
+    new_ini_file.write(file_ini_content)
+    new_ini_file.close()
+            
+    return new_ini_file_name
--- ini/global_run/gcm_ssp_runs/runDynQual_global_on_snellius.sh
+++ ini/global_run/gcm_ssp_runs/runDynQual_global_on_snellius.sh
@@ -9,6 +9,9 @@
 # we use 64 cores (1/3 of genoa node, maximum memory: 112 GB)
 #SBATCH -n 64
 
+#SBATCH -t 119:59:00
+## this is the time, maximum 120:00:00 hours
+
 # job name
 #SBATCH -J dynqual
 
@@ -23,10 +26,10 @@
 
 
 # please set where you stored DYNQUAL scripts
-SCRIPT_FOLDER=$HOME/github/Graha010/DYNQUAL/DynQualModel/
+SCRIPT_FOLDER=/home/edwinoxy/github/Graha010/DYNQUAL/DynQualModel/
 
 # configuration (.ini) file
-INI_FILE=$HOME/DYNQUAL/ini/global_run/gcm_ssp_runs/DynQual_05min_global_gcm.ini
+INI_FILE=/home/edwinoxy/DYNQUAL/ini/global_run/gcm_ssp_runs/DynQual_05min_global_gcm.ini
 
 
 # we activate the correct conda environment on eejit and many other settings
@@ -43,8 +46,26 @@ source activate /home/hydrowld/.conda/envs/pcrglobwb_python3_2023-10-31
 # we go to the folder where the folder script
 cd ${SCRIPT_FOLDER}
 
+
 # global online run with parallelization
-python parallel_pcrglobwb_runner_with_argument.py ${INI_FILE} -mod ${OUTPUT_FOLDER} -sd ${START_DATE} -ed ${END_DATE} -misd ${INITIAL_CONDITION_FOLDER} -dfis {DATE_FOR_INITIAL_STATES} -pff ${PRE_FILE} -tff ${TMP_FILE} -rpetff ${ET0_FILE} -radff ${RAD_FILE} -tmp_annavg_ff ${TMP_ANNUAL_AVG_FILE} -irr_fl ${IRRIGATION_AREA_FILE} -dom_fl ${DOM_WATER_DEMAND_FILE} -ind_fl ${IND_WATER_DEMAND_FILE}
+
+#~ # - old method - DO NOT USE IT!
+#~ python parallel_pcrglobwb_runner_with_argument.py ${INI_FILE} -mod ${OUTPUT_FOLDER} -sd ${START_DATE} -ed ${END_DATE} -misd ${INITIAL_CONDITION_FOLDER} -dfis ${DATE_FOR_INITIAL_STATES} -pff ${PRE_FILE} -tff ${TMP_FILE} -rpetff ${ET0_FILE} -radff ${RAD_FILE} -tmp_annavg_ff ${TMP_ANNUAL_AVG_FILE} -irr_fl ${IRRIGATION_AREA_FILE} -dom_fl ${DOM_WATER_DEMAND_FILE} -ind_fl ${IND_WATER_DEMAND_FILE}
+
+# - for testing
+for i in {2..4}
+#~ # - loop through all clones
+#~ for i in {1..53}
+
+do
+
+CLONE_CODE=${i}
+python deterministic_runner_new.py ${INI_FILE} debug_parallel ${CLONE_CODE} -mod ${OUTPUT_FOLDER} -sd ${START_DATE} -ed ${END_DATE} -misd ${INITIAL_CONDITION_FOLDER} -dfis ${DATE_FOR_INITIAL_STATES} -pff ${PRE_FILE} -tff ${TMP_FILE} -rpetff ${ET0_FILE} -radff ${RAD_FILE} -tmp_annavg_ff ${TMP_ANNUAL_AVG_FILE} -irr_fl ${IRRIGATION_AREA_FILE} -dom_fl ${DOM_WATER_DEMAND_FILE} -ind_fl ${IND_WATER_DEMAND_FILE} &
+
+done
+
+# - wait until all clone runs are finished
+wait
 
 # merging the states from the last date
 python merge_pcraster_maps.py ${END_DATE} ${OUTPUT_FOLDER} ${OUTPUT_FOLDER}/global/ 8 Global 
